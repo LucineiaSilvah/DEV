08721b28124e172736506f6aec8c11001b3cf1d4
luh <lucineia.rsilvah@gmail.com>
2023-09-05 21:46:14 -0300

    atualizando menu

---
+++ assets/js/main.js
@@ -0,0 +1,38 @@
+const botao = document.querySelector('.fa-angles-up');
+const corpo = document.querySelector('html')
+
+document.addEventListener('scroll', vereficar)
+
+function vereficar(){
+  if(scrollY > 400){
+    botao.style.visibility = 'visible'
+  }else{
+    botao.style.visibility = 'hidden'
+  }
+}
+
+
+botao.addEventListener('click',()=>{
+  window.scrollTo({
+    top:0
+  })
+})
+
+let menu = document.querySelector('.fa-bars');
+menu.addEventListener('click', ()=>{
+  let menuLista = document.querySelector('.menu__lista')
+  menuLista.classList.toggle('mostrarMenu');
+  
+})
+
+let modo = document.getElementById('mode')
+ 
+ modo.addEventListener('click', ()=> {
+   document.querySelector('.container').classList.toggle('white')
+  
+ document.querySelector('body').classList.toggle('white')
+
+
+ })
+ 
+ 
